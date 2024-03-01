23f9be669a2d9b18c752897d61892e68e86d8e69
Tiarles Moralles Guterres <tiarles.moralles@typhoon-hil.com>
2024-03-01 20:36:42 +0100

    Renaming project 0 folder, changing google image search and adding Django material (WIP)

---
+++ 1_django_material/django_commands.cmd
@@ -0,0 +1,8 @@
+rem To start a Django project
+django-admin startproject lecture3
+
+cd lecture3
+
+rem More interesting files there: settings.py, urls.py
+rem The manage.py is used to execute some django commands, like:
+python manage.py runserver
