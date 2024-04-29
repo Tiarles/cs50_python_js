8572f47d9e6536004b4e231abf24f3b1cab9756b
Tiarles Moralles Guterres <tiarles.moralles@typhoon-hil.com>
2024-04-29 22:01:23 +0200

    Finishing Routes section implementation

--- 1_django_material/lecture3/hello/urls.py
+++ 1_django_material/lecture3/hello/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
+    path("tiarles", views.tiarles, name="tiarles"),
+    path("david", views.david, name="david"),
 ]
--- 1_django_material/lecture3/hello/views.py
+++ 1_django_material/lecture3/hello/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world!")
+
+def tiarles(request):
+    return HttpResponse("Hello, Tiarles!")
+
+def david(request):
+    return HttpResponse("Hello, David!")
+
+def greet(request, name: str):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
