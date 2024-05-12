eed1af32e4352bc4f95c6f478f1d7e1fc4214535
Tiarles Guterres <tiarlesmoralles@hotmail.com>
2024-05-12 10:48:55 +0200

    Changing module of markdown regex func

--- 0_project_1/wiki/encyclopedia/util.py
+++ 0_project_1/wiki/encyclopedia/util.py
@@ -43,3 +43,43 @@ def get_entry(title):
         return ret
     except FileNotFoundError:
         return None
+
+
+def markdown_to_html_body(file_md_path, verbose=False):
+    import re
+    import os
+
+    # folder_path = os.path.dirname(file_md_path)
+    title = os.path.basename(file_md_path)
+
+    patterns = {
+        "h1": (re.compile('^# (.*)', re.M), r'<h1>\1</h1>'),
+        "h2": (re.compile('^## (.*)', re.M), r'<h2>\1</h2>'),
+        "h3": (re.compile('^### (.*)', re.M), r'<h3>\1</h3>'),
+        "h4": (re.compile('^#### (.*)', re.M), r'<h4>\1</h4>'),
+        "h5": (re.compile('^##### (.*)', re.M), r'<h5>\1</h5>'),
+        "h6": (re.compile('^###### (.*)', re.M), r'<h6>\1</h6>'),
+        "a": (re.compile(R'\[(.*?)\]\((.*?)\)', re.M), r'<a href="\2">\1</a>')
+    }
+
+    with open(file_md_path, 'r') as fmd:
+        filestr = fmd.read()
+
+    if verbose:
+        print("Markdown:")
+        print(filestr)
+
+    file_html_body = filestr
+
+    for pattern, sub_str in patterns.values():
+        file_html_body = pattern.sub(sub_str, file_html_body)
+
+    if verbose:
+        print("\nHTML:")
+        print(file_html_body)
+
+    with open(file_md_path, 'w') as fmd:
+        filestr = fmd.read()
+
+
+    return file_html_body, title
