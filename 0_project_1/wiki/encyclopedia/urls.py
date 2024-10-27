39a1c0baea23e17ef96838a577daeb1d49d09661
Tiarles Guterres <tiarlesmoralles@hotmail.com>
2024-10-27 19:22:09 +0100

    Stable modification on edit_page

--- 0_project_1/wiki/encyclopedia/urls.py
+++ 0_project_1/wiki/encyclopedia/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path("new", views.new_page, name="new_page"),
     path("random", views.random_page, name="random_page"),
     path("tutorial_spec", views.tutorial_spec, name="tutorial_spec"),
+    path("edit", views.edit_page, name="edit_page"),
 
     path("<str:title>", views.entries, name="wiki_pages"),
 ]
