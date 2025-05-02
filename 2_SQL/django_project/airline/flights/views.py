a851d2102d0afe3af227d96b8049f7aaeefb5d2e
Tiarles Guterres <tiarlesmoralles@hotmail.com>
2025-05-02 16:55:05 +0200

    Adding passenger list on flights page and setup command

--- 2_SQL/django_project/airline/flights/views.py
+++ 2_SQL/django_project/airline/flights/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, HttpResponse
-
-from .models import Flight, Airport
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from .models import Flight, Airport, Passenger
 
 
 def index(request):
@@ -11,7 +11,22 @@ def index(request):
 
 def flight(request, flight_id):
     flight = Flight.objects.get(id=flight_id)
+    passengers = flight.passengers.all()
+    non_passengers = Passenger.objects.exclude(flights=flight).all()
 
     return render(request, "flights/flights.html", {
-        "flight": flight, 
+        "flight": flight,
+        "passengers": passengers,
+        "non_passengers": non_passengers
     })
+
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(id=flight_id)
+        passenger_id = int(request.POST["passenger"])
+        passenger = Passenger.objects.get(pk=passenger_id)
+
+        passenger.flights.add(flight)
+
+        return HttpResponseRedirect(reverse("flights", args=(flight.id,)))
--- 2_SQL/django_project/airline/migrate_make.ps1
+++ 2_SQL/django_project/airline/migrate_make.ps1
@@ -1,4 +1,3 @@
 ..\..\..\venv\Scripts\activate
-python .\manage.py migrate
 python .\manage.py makemigrations
 python .\manage.py migrate
